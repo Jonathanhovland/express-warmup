4258220501396dd2dfca25d0ecc635d988bb96f6
jonathanhovland <jonathan.hovland@yahoo.com>
2018-10-23 09:58:44 -0600

    Working on getting the individual Id's to work

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const port = 3053
+const cakes = require("./data.json")
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get("/cakes", (req, res) => {
+    res.json({ cakes: cakes })
+})
+
+app.get("/cakes/:id", (req, res) => {
+    const id = (req.params.id)
+
+    const cakes = cakes.filter(cakes => {
+        return cakes.id == id
+    })[0]
+
+    res.json(cakes)
+})
+
+app.use(notFound)
+app.use(errorHandler)
+
+function notFound(req, res, next) {
+  res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+}
+
+function errorHandler(err, req, res, next) {
+    console.error('ERROR', err)
+    const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+    res.status(500).send({error: err.message, stack, url: req.originalUrl})
+  }
+
+
+app.listen(port, () => console.log("server is running on 3053"))
